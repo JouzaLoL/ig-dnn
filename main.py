dd55f18f21c285e0175fc063ae9996e4dc4bc47e
Josef Vacek <vacekj@outlook.com>
2019-06-10 09:18:32 +0200

    leaky relus

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from keras.layers import Embedding
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import LeakyReLU
 from keras.optimizers import SGD
 import keras as keras
 import numpy as np
@@ -15,33 +16,32 @@ from matplotlib import pyplot
 from keras import backend as K
 K.tensorflow_backend._get_available_gpus()
 
-# Data 
+# Data
 # input structure: [minutes since midnight, likes, comments]
 dataname = sys.argv[1] if len(sys.argv) > 1 != None else 'spacex.csv'
 data = np.genfromtxt(dataname, delimiter=',')
 
 # Fix data to minutes and discard first value
-data[:,0] = data[:,0] * 100
+data[:, 0] = data[:, 0] * 100
 data = data[1:]
 
 
 # Sort data
-data = data[data[:,0].argsort()]
+data = data[data[:, 0].argsort()]
 
 # Plot Raw training data
 pyplot.subplot(211)
 pyplot.title("Raw training data")
-pyplot.plot(data[:,0], data[:,1], 'bo', label="likes")
+pyplot.plot(data[:, 0], data[:, 1], 'bo', label="likes")
 pyplot.legend()
 pyplot.subplot(212)
-pyplot.plot(data[:,0], data[:,2], 'go', label="comments")
+pyplot.plot(data[:, 0], data[:, 2], 'go', label="comments")
 pyplot.legend()
 pyplot.show()
 
 # Split data into 10-minute clusters
 # https://stackoverflow.com/questions/11767139/split-numpy-array-at-multiple-values
-indices = np.array(range(0,1440, 60))
-print(indices)
+indices = np.array(range(0, 1440, 60))
 
 split_at = data[:, 0].searchsorted(indices)
 split_data = np.split(data, split_at)
@@ -58,10 +58,10 @@ data_avgs = data_avgs[~np.isnan(data_avgs).any(axis=1)]
 # Plot grouped training data
 pyplot.subplot(211)
 pyplot.title("Grouped avg training data")
-pyplot.plot(data_avgs[:,0], data_avgs[:,1], 'bo', label="likes")
+pyplot.plot(data_avgs[:, 0], data_avgs[:, 1], 'bo', label="likes")
 pyplot.legend()
 pyplot.subplot(212)
-pyplot.plot(data_avgs[:,0], data_avgs[:,2], 'go', label="comments")
+pyplot.plot(data_avgs[:, 0], data_avgs[:, 2], 'go', label="comments")
 pyplot.legend()
 pyplot.show()
 
@@ -86,19 +86,27 @@ tensorboardCallback = keras.callbacks.TensorBoard(log_dir='./graph/' + dataname 
 model = Sequential()
 
 # Input layer
-model.add(Dense(8, input_dim=1,
+model.add(Dense(64, input_dim=1,
                 kernel_initializer='glorot_normal', activation='relu'))
 
 # Hidden layers
-model.add(Dense(32, activation='relu'))
+model.add(Dense(64))
+model.add(LeakyReLU(alpha=0.1))
+# model.add(Dropout(0.5))
+model.add(Dense(64))
+model.add(LeakyReLU(alpha=0.1))
+# model.add(Dropout(0.5))
+model.add(Dense(64))
+model.add(LeakyReLU(alpha=0.1))
+# model.add(Dropout(0.5))
 
 # Output layer
 model.add(Dense(1, kernel_initializer='normal'))
 
 model.compile(loss='mean_squared_error',
-              optimizer=keras.optimizers.Adam(lr=0.01))
+              optimizer=keras.optimizers.Adam(), metrics=["mean_absolute_percentage_error", "mean_absolute_error"])
 
-history = model.fit(x_train, y_train, batch_size=1, epochs=300, shuffle=True,
+history = model.fit(x_train, y_train, batch_size=1, epochs=3000,
                     callbacks=[tensorboardCallback])
 
 # plot loss during training
